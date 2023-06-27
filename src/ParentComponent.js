4f4db09cd3aa517c9d249e404d6c3864a81d133a
Akshay Chaudhary <124782758+AkshayChaudharyctrl@users.noreply.github.com>
2023-06-27 06:08:55 +0000

    final

--- src/ParentComponent.js
+++ src/ParentComponent.js
@@ -24,24 +24,26 @@ function ParentComponent() {
       <ChildComponent todos={todos} handleComplete={handleComplete} />
     </div>
   );
+  
+  function ChildComponent({ todos, handleComplete }) {
+    return (
+      <div>
+        <h3>Child Component</h3>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}{' '}
+              {!todo.completed && (
+                <button onClick={() => handleComplete(todo.id)}>Complete</button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
-function ChildComponent({ todos, handleComplete }) {
-  return (
-    <div>
-      <h3>Child Component</h3>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.text}{' '}
-            {!todo.completed && (
-              <button onClick={() => handleComplete(todo.id)}>Complete</button>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+
 
 export default ParentComponent;
